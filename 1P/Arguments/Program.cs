2381237383887e1f1ad97cb7cbe3886ca2459350
AngelBrambila91 <malthael.91@gmail.com>
2023-08-16 08:50:26 -0600

    Added Projects of formatting and Arguments

---
+++ 1P/Arguments/Program.cs
@@ -0,0 +1,32 @@
+﻿WriteLine($"There are {args.Length} arguments");
+
+foreach (var argument in args)
+{
+    WriteLine(argument);
+}
+
+if(args.Length < 3)
+{
+    WriteLine("You must enter at least 2 colors and cursor size e.g: ");
+    WriteLine("dotnet run red yellow 50");
+    return; //stop Running
+}
+// first color
+ForegroundColor = (ConsoleColor)Enum.Parse(
+    enumType:typeof(ConsoleColor), 
+    value:args[0],
+    ignoreCase: true);
+// second color
+BackgroundColor = (ConsoleColor)Enum.Parse(
+    enumType:typeof(ConsoleColor),
+    value:args[1],
+    ignoreCase: true);
+// cursor size
+try
+{
+    CursorSize = int.Parse(args[2]);
+}
+catch (PlatformNotSupportedException)
+{
+    WriteLine("The current platform does not support changing the size of the cursor");
+}
---
+++ 1P/Formatting/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+// Margins
+// { index [,alignment] numeric [: format String]}
+string strawberryText = "Strawberry";
+int strawberryCount = 1234;
+string mangoText = "Mango";
+int mangoCount = 43000;
+// Headers
+WriteLine("{0,-10} {1,6}",
+arg0: "Name",
+arg1: "Count");
+// 1st Row
+WriteLine("{0,-10} {1,6:N0}",
+arg0: strawberryText,
+arg1: strawberryCount);
+// 2nd Row
+WriteLine("{0,-10} {1,6:N0}",
+arg0: mangoText,
+arg1: mangoCount);
+
+#region Getting Input from User
+    Write("Type your first name and press Enter: ");
+    string firstName = ReadLine();
+    Write("Type your age and press Enter: ");
+    string age = ReadLine();
+    WriteLine($"You look good to for {age}");
+#endregion
+
+#region Infer Types
+    // Pros : Can transfer data quickly
+    // Cons : Do not infer the methods and properties
+    object objectNumber = 13;
+    WriteLine(objectNumber.GetType());
+
+    // Pros : Allocates memory on CPU
+    // Cons : Don't deallocate the memory
+    dynamic nameCaps = "MAURICIOOOOOOO";
+    
+    // Pros : Until definition and usage, allocates memory on RAM
+    // Cons : Really complex types can confuse var
+    var  nameLower = "diego";
+#endregion
