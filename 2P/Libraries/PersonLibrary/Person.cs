00d18a94a1692eb3033a9cfd0ebc7b5f36d47abc
AngelBrambila91 <malthael.91@gmail.com>
2023-09-18 10:57:15 -0600

    Added OOP and PersonLibraryProject

---
+++ 2P/Libraries/PersonLibrary/Person.cs
@@ -0,0 +1,30 @@
+﻿namespace PA17B.Shared;
+
+public class Person
+{
+    /*
+     Constructor:  its called whe
+     new () is executed, instance
+
+     Member : data inside the class
+     public int number = 0;
+     public int getNumber () { return this.number };
+     public void setNumber (int number)
+     {
+        thisk.number = number;
+     }
+
+     Property: Still data on the class
+     string Name {get; set;}
+     Simple Property
+     Full Property
+
+     Index : []
+
+     Operators : + - & ^ | =>
+    */
+
+    //fields
+    public string? Name;
+    public DateTime DateOfBirth;
+}
---
+++ 2P/OOP/PeopleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using PA17B.Shared;
+
+Person kaleb = new();
+WriteLine($"Kaleb is : {kaleb.ToString()} ");
+
+kaleb.Name = "Kaleb";
+kaleb.DateOfBirth = new DateTime(2005, 12, 21);
+WriteLine(format: "{0} was born on {1:dddd, d MMMM yyyy}",
+arg0:kaleb.Name,
+arg1:kaleb.DateOfBirth);
